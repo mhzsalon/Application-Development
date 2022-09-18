8600aca233562238ae56edfc451ddf04c793e8aa
mhzsalon <106007614+mhzsalon@users.noreply.github.com>
2022-09-18 22:32:01 +0545

    first comit

---
+++ SecondConsole/SecondClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondConsole
+{
+    public class SecondClass
+    {
+        public string secondMethod() {
+            return "second message";
+        }
+    }
+}
---
+++ firstConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using firstConsoleApp;
+using SecondConsole;
+
+Console.WriteLine("Hello, World!");
+
+firstClass obj = new firstClass();
+var first = obj.firstMethod();
+
+SecondClass obj2 = new SecondClass();
+var second = obj2.secondMethod();
+
+Console.WriteLine(first);
+Console.WriteLine(second);
+Console.ReadLine();
